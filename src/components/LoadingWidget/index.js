7b7eeddc83d14efb20a63c3edf8ca1fd7dbe2aaa
Patrick Oliveira <patrickoliveira58@gmail.com>
2021-02-06 19:18:13 -0300

    Alteração Loading

--- src/components/LoadingWidget/index.js
+++ src/components/LoadingWidget/index.js
@@ -1,20 +1,23 @@
 import styled from "styled-components";
 import QuizBackground from "../QuizBackground";
+import Widget from "../Widget";
 
 const Loading = styled.div`
-  width: 100%;
-  height: 100%;
-  background-size: 100vw 100vh;
+  width: 260px;
+  height: 200px;
   background-position: center;
   background-repeat: no-repeat;
-  background-image: url("https://media.giphy.com/media/TvLuZ00OIADoQ/giphy.gif");
+  background-image: url("https://media.giphy.com/media/XZL090E18RxixkON2U/giphy.gif");
 `;
 
 function LoadingWidget() {
   return (
-    <QuizBackground.Loading backgroundImage="none">
-      <Loading></Loading>
-    </QuizBackground.Loading>
+    <Widget>
+      <Widget.Header>Só um momento</Widget.Header>
+      <Widget.Content>
+        <Loading></Loading>
+      </Widget.Content>
+    </Widget>
   );
 }
 
